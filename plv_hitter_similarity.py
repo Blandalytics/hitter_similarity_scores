46ed34ec0029481eaf34d31f4cccd67116f2d9cd
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-12-13 15:55:49 -0700

    Update plv_hitter_similarity.py

--- plv_hitter_similarity.py
+++ plv_hitter_similarity.py
@@ -21,18 +21,18 @@ st.image(logo, width=200)
 
 ## Set Styling
 # Plot Style
-pl_white = '#FEFEFE'
+pl_white = '#FFFFFF'
 pl_background = '#292C42'
 pl_text = '#72a3f7'
-pl_line_color = '#293a6b'
+pl_line_color = '#8D96B3'
 
 sns.set_theme(
     style={
-        'axes.edgecolor': pl_white,
+        'axes.edgecolor': pl_line_color,
         'axes.facecolor': pl_background,
         'axes.labelcolor': pl_white,
-        'xtick.color': pl_white,
-        'ytick.color': pl_white,
+        'xtick.color': pl_line_color,
+        'ytick.color': pl_line_color,
         'figure.facecolor':pl_background,
         'grid.color': pl_background,
         'grid.linestyle': '-',
@@ -140,8 +140,8 @@ def generate_comp_card(player_stats, sim_stats, top_comps,top=True):
     
     bar_hues = ['#872B2C','#0C3E9B','#286F04','#57219B','#B1550D','#794C30']
     sns.barplot(chart_df,
-                x='variable',y='value',hue='label_text',palette=bar_hues,#'Set1',
-               edgecolor='#8D96B3',linewidth=0.5, alpha=1,ax=axs[0]
+                x='variable',y='value',hue='label_text',palette='Set1',
+               edgecolor=pl_background,linewidth=1, alpha=1,ax=axs[0]
                )
     axs[0].axhline(100,alpha=1,linestyle='--',color=pl_white,xmax=0.98)
     y_diff_max = max(abs(chart_df['value'].max()-100),abs(100-chart_df['value'].min()))+2
