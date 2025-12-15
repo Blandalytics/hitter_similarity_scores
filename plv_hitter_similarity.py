3dd05690f76c53aa3965e4b7a9051da3c9185503
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-12-14 22:48:07 -0800

    Update plv_hitter_similarity.py

--- plv_hitter_similarity.py
+++ plv_hitter_similarity.py
@@ -209,7 +209,7 @@ def generate_comp_card(player_stats, sim_stats, top_comps,top=True):
     axs[1].axis('off')
 
     # fig.suptitle(f"{top_comps.iloc[0]['Name']}'s {sim_season} Skill Similarity Scores{least_text}",y=1.05,color=pl_text,fontsize=20)
-    fig.text(0.1625,1.025,f"Skill Similarity\nScores{least_text}",va='center',ha='left',color=pl_text,fontsize=16)
+    fig.text(0.16,1.025,f"Hitter Skill\nSimilarity Scores{least_text}",va='center',ha='left',color=pl_text,fontsize=16)
     fig.text(0.345,1.025,f"{top_comps.iloc[0]['Name']} ({sim_season})",va='center',ha='left',color=pl_highglight,fontsize=24)
     # Add PL logo
     pl_ax = fig.add_axes([0.08,0.97,0.15,0.12], anchor='SW', zorder=1)
